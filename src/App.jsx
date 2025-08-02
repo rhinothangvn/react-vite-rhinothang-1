e49cb9b42d4a615b2d1fbae76a72c5dfdf210e6a
rhinothangvn <rhinothangvn@outlook.com>
2025-08-02 14:18:21 +0700

    commit 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//() => {}
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>Hello from MyComponent!</h2>
+    </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +26,7 @@ function App() {
         </a>
       </div>
       <h1>Hello World + From Rhino Thang</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
