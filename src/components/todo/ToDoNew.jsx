7ff4e7941fc6e9ff1c7a65ffd6d1949d01bf968d
rhinothangvn <rhinothangvn@outlook.com>
2025-08-03 23:21:11 +0700

    commit 36

--- src/components/todo/ToDoNew.jsx
+++ src/components/todo/ToDoNew.jsx
@@ -1,13 +1,26 @@
 const ToDoNew = (props) => {
-    console.log(">>> check poit:", props);
+  console.log(">>> check poit:", props);
 
-    const { addNewToDo } = props;
+  const { addNewToDo } = props;
 
-    // addNewToDo("Rhino Thang");
+  // addNewToDo("Rhino Thang");
+  const handleClick = () => {
+    alert("Add new task");
+    addNewToDo("Rhino Thang");
+  };
+
+const handleOnChange = (name) => {
+    console.log(">>> check handle onChange",name);
+}
+
   return (
     <div className="todo-new">
-      <input type="text" placeholder="Add a new task" title="Add a new task" />
-      <button>Add</button>
+      <input type="text" placeholder="Add a new task" title="Add a new task"  
+      onChange={(event) => handleOnChange(event.target.value)}/>
+      <button style={{ cursor: "pointer" }} 
+      onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
