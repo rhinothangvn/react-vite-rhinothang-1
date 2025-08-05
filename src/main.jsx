d865cb1b9faf4d368f57f4bbc62d156f23274bef
rhinothangvn <rhinothangvn@outlook.com>
2025-08-05 15:51:38 +0700

    commit 47

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login Page</div>
+  },
+  {
+    path: "/resgister",
+    element: <div>Register Page</div>
+  },
+  {
+    path: "/user",
+    element: <div>User Page</div>
+  },
+{
+    path: "/product",
+    element: <div>Product Page</div>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
