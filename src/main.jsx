82c5ca1a5b8f9964bfadd4d29381a10676cfe86a
rhinothangvn <rhinothangvn@outlook.com>
2025-08-05 23:04:05 +0700

    commit 52

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/login.jsx";
 import RegisterPage from "./pages/register.jsx";
 import UserPage from "./pages/user.jsx";
-import ProductPage from "./pages/product.jsx";
+import BookPage from "./pages/book.jsx";
 import "./styles/global.css";
 import TodoApp from "./components/todo/TodoApp.jsx";
 import "./components/todo/todo.css";
+import ErrorPage from "./pages/error.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -25,7 +27,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/products",
-        element: <ProductPage />,
+        element: <BookPage />,
       },
     ],
   },
